8b1ccf96c38f6356a57bffdfa5b2183f08faf214
MarinaFerencak <96020683+MarinaFerencak@users.noreply.github.com>
2022-07-16 10:38:43 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Marina Ferenčak and it's{" "}
+        <a
+          href="https://github.com/MarinaFerencak/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
